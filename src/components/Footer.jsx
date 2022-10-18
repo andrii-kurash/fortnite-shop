04508e5d3fbf64106d0caa922f2fab63e2dd0c58
Andrii <kurash.andrii@gmail.com>
2022-10-18 16:46:28 +0300

    uncompleted version of fortnite shop

---
+++ src/components/Footer.jsx
@@ -0,0 +1,16 @@
+const Footer = () => {
+    return (
+        <footer className="page-footer deep-purple lighten-2">
+            <div className="footer-copyright">
+                <div className="container">
+                    © 2022 Copyright Text
+                    <a className="grey-text text-lighten-4 right" href="https://github.com/andrii-kurash/fortnite-shop" target="_blank" rel="noreferrer">
+                        Repo
+                    </a>
+                </div>
+            </div>
+        </footer>
+    );
+};
+
+export { Footer };
---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+const Header = () => {
+    return (
+        <div>
+            <nav className="deep-purple darken-1">
+                <div className="nav-wrapper">
+                    <a href="!#" className="brand-logo">Fortnite Shop</a>
+                    <ul id="nav-mobile" className="right hide-on-med-and-down">
+                        <li><a href="https://github.com/andrii-kurash/fortnite-shop" target="_blank" rel="noreferrer">Repo</a></li>
+                    </ul>
+                </div>
+            </nav>
+        </div >
+    );
+};
+
+export { Header };
